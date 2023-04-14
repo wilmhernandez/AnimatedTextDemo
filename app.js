e72ff8237e554e6eb41012480d6e1db450187479
wilmhernandez <hernandez.wilbert.m@gmail.com>
2023-04-14 10:17:40 -0700

    add v1 of color script

---
+++ app.js
@@ -0,0 +1,14 @@
+const randomRGB = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+const letters = document.querySelectorAll('.letter');
+
+const intervalId = setInterval ( ()=>{
+  for (let letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 1000)
